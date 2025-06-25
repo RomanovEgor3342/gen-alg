dbf2326278d9573c66447668f5819f17e04faec6
Amir Galeev <amir29032005@icloud.com>
2025-06-25 16:11:40 +0300

    main create 2

--- src/main.py
+++ src/main.py
@@ -1,4 +1,7 @@
-from Modules.solution import Solution
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5 import QtWidgets
+import random
+import sys
 
 FIELD_SIZE = 9
 POPULATION_SIZE = 200
@@ -6,6 +9,36 @@ P_CROSSOVER = 0.5
 P_MUTATION = 0.5
 MAX_GENERATIONS = 150
 
+
+class Solution:
+    def __init__(self, id=int, generation=int):
+        self.id = id
+        self.generation = generation
+        self.field = [[0 for _ in range(FIELD_SIZE)] for _ in range(FIELD_SIZE)]
+
+
+# def evaluate():
+# def crossover():
+# def grade():
+
+def application():
+    app = QApplication(sys.argv)
+    window = QMainWindow()
+
+    window.setWindowTitle("ГА Судоку")
+    window.setGeometry(300, 250, 350, 200)
+
+    start_btn = QtWidgets.QPushButton(window)
+    start_btn.setText("Запуск")
+    start_btn.setFixedWidth(200)
+    start_btn.clicked.connect(main)
+
+    window.show()
+    sys.exit(app.exec_())
+
+
+def main():
+    print('ЗАПУЩЕНО')
+
 if __name__ == "__main__":
-    sol = Solution(1, 3, FIELD_SIZE)
-    print(sol.id)
+    application()
