03c3fae4ea6a8fc593bd06ad71be5023a7154fb7
KHAIRULLOV7DINAR <khairullovdinarr@gmail.com>
2025-06-28 23:52:24 +0300

    minor changes

--- src/sudoku_field.py
+++ src/sudoku_field.py
@@ -4,7 +4,8 @@ import numpy as np
 class FieldCreator():
     def __init__(self):
         self.main_permutation = list(range(1, 10)) * 9
-        self.insert_list = []
+        self.insert_list_indexes = []
+        self.insert_list_symbols = []
     def ReadFromFile(self, file_name: str) -> None:
         with open(file_name, 'r') as file:
             file_field = [item.split(' ') for item in file.read().split('\n')]
@@ -13,7 +14,8 @@ class FieldCreator():
             for y in range(9):
                 symbol = file_field[x][y]
                 if symbol != 'x':
-                    self.insert_list.append((x, y, int(symbol)))
+                    self.insert_list_indexes.append((x, y))
+                    self.insert_list_symbols.append(int(symbol))
                     self.main_permutation.remove(int(symbol))
         
 
@@ -21,8 +23,8 @@ class FieldCreator():
         population = []
         for _ in range(entities_amount):
             new_entity = np.random.permutation(self.main_permutation).tolist()
-            for item in self.insert_list:
-                new_entity.insert(item[0] * 9 + item[1], item[2])
+            for index in range(len(self.insert_list_symbols)):
+                new_entity.insert(self.insert_list_indexes[index][0] * 9 + self.insert_list_indexes[index][1], self.insert_list_symbols[index])
             new_entity = [new_entity[i:i + 9] for i in range(0, 81, 9)]
             population.append(new_entity)
 
