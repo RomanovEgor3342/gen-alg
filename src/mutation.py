6be5c0cef679ca508a188729c2e9c92246f295f9
KHAIRULLOV7DINAR <khairullovdinarr@gmail.com>
2025-06-30 22:11:49 +0300

    another mutation function

--- src/mutation.py
+++ src/mutation.py
@@ -25,46 +25,30 @@ def random_mutation(entity: list, insert_list_indexes: list) -> None:
     entity[first_x][first_y] = entity[second_x][second_y]
     entity[second_x][second_y] = sym
 
-# def row_mutation(entity: list) -> None:
-#     numbers = list(range(0, 9))
-#     x = random.choice(numbers)
-#     first_y = random.choice(numbers)
-#     numbers.remove(first_y)
-#     second_y = random.choice(numbers)
-#
-#     sym = entity[x][first_y]
-#     entity[x][first_y] = entity[x][second_y]
-#     entity[x][second_y] = sym
-#
-#
-# def column_mutation(entity: list) -> None:
-#     numbers = list(range(0, 9))
-#     y = random.choice(numbers)
-#     first_x = random.choice(numbers)
-#     numbers.remove(first_x)
-#     second_x = random.choice(numbers)
-#
-#     sym = entity[first_x][y]
-#     entity[first_x][y] = entity[second_x][y]
-#     entity[second_x][y] = sym
+def mutation_among_bad_rows(entity: list, insert_list_indexes: list, bad_rows_indexes: list, row_column_flag: bool) -> None:
+    if row_column_flag == True:
+        numbers = []
+        for item in bad_rows_indexes:
+            numbers += list(range(item * 9, item * 9 + 9))
 
-# def random_mutation(entity: list, insert_list_indexes: list) -> None:
-#     numbers = list(range(0, 9))
-#     first_x = random.choice(numbers)
-#     second_x = random.choice(numbers)
-#
-#     first_y = random.choice(numbers)
-#
-#     if first_x == second_x:
-#         second_y = random.choice(numbers)
-#
-#     else:
-#         numbers.remove(first_y)
-#         second_y = random.choice(numbers)
-#
-#     sym = entity[first_x][first_y]
-#     entity[first_x][first_y] = entity[second_x][second_y]
-#     entity[second_x][second_y] = sym
+        for item in insert_list_indexes:
+            if item[0] in bad_rows_indexes:
+                numbers.remove(item[0] * 9 + item[1])
+
+        first_sum = random.choice(numbers)
+        numbers.remove(first_sum)
+        second_sum = random.choice(numbers)
+
+        first_x = first_sum // 9
+        first_y = first_sum % 9
+
+        second_x = second_sum // 9
+        second_y = second_sum % 9
+
+        sym = entity[first_x][first_y]
+        entity[first_x][first_y] = entity[second_x][second_y]
+        entity[second_x][second_y] = sym
+
 
 
 
