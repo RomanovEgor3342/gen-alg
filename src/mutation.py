a65b52fe14e98bd835a2f3d9ba7a77bb2ae036c4
KHAIRULLOV7DINAR <khairullovdinarr@gmail.com>
2025-06-30 21:00:40 +0300

    changed mutation function

--- src/mutation.py
+++ src/mutation.py
@@ -4,44 +4,67 @@ import random
 Мутация 2 рандомных клеток в одном рандомном столбце
 Мутация 2 абсолютно рандомных клеток
 """
-def row_mutation(entity: list) -> None:
-    numbers = list(range(0, 9))
-    x = random.choice(numbers)
-    first_y = random.choice(numbers)
-    numbers.remove(first_y)
-    second_y = random.choice(numbers)
-
-    sym = entity[x][first_y]
-    entity[x][first_y] = entity[x][second_y]
-    entity[x][second_y] = sym
 
 
-def column_mutation(entity: list) -> None:
-    numbers = list(range(0, 9))
-    y = random.choice(numbers)
-    first_x = random.choice(numbers)
-    numbers.remove(first_x)
-    second_x = random.choice(numbers)
+def random_mutation(entity: list, insert_list_indexes: list) -> None:
+    numbers = list(range(0, 81))
+    for item in insert_list_indexes:
+        numbers.remove(item[0] * 9 + item[1])
 
-    sym = entity[first_x][y]
-    entity[first_x][y] = entity[second_x][y]
-    entity[second_x][y] = sym
+    first_sum = random.choice(numbers)
+    numbers.remove(first_sum)
+    second_sum = random.choice(numbers)
 
-def random_mutation(entity: list) -> None:
-    numbers = list(range(0, 9))
-    first_x = random.choice(numbers)
-    second_x = random.choice(numbers)
+    first_x = first_sum // 9
+    first_y = first_sum % 9
 
-    first_y = random.choice(numbers)
-
-    if first_x == second_x:
-        second_y = random.choice(numbers)
-
-    else:
-        numbers.remove(first_y)
-        second_y = random.choice(numbers)
+    second_x = second_sum // 9
+    second_y = second_sum % 9
 
     sym = entity[first_x][first_y]
     entity[first_x][first_y] = entity[second_x][second_y]
     entity[second_x][second_y] = sym
+
+# def row_mutation(entity: list) -> None:
+#     numbers = list(range(0, 9))
+#     x = random.choice(numbers)
+#     first_y = random.choice(numbers)
+#     numbers.remove(first_y)
+#     second_y = random.choice(numbers)
+#
+#     sym = entity[x][first_y]
+#     entity[x][first_y] = entity[x][second_y]
+#     entity[x][second_y] = sym
+#
+#
+# def column_mutation(entity: list) -> None:
+#     numbers = list(range(0, 9))
+#     y = random.choice(numbers)
+#     first_x = random.choice(numbers)
+#     numbers.remove(first_x)
+#     second_x = random.choice(numbers)
+#
+#     sym = entity[first_x][y]
+#     entity[first_x][y] = entity[second_x][y]
+#     entity[second_x][y] = sym
+
+# def random_mutation(entity: list, insert_list_indexes: list) -> None:
+#     numbers = list(range(0, 9))
+#     first_x = random.choice(numbers)
+#     second_x = random.choice(numbers)
+#
+#     first_y = random.choice(numbers)
+#
+#     if first_x == second_x:
+#         second_y = random.choice(numbers)
+#
+#     else:
+#         numbers.remove(first_y)
+#         second_y = random.choice(numbers)
+#
+#     sym = entity[first_x][first_y]
+#     entity[first_x][first_y] = entity[second_x][second_y]
+#     entity[second_x][second_y] = sym
+
+
 
